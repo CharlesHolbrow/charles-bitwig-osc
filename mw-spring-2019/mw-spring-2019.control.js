1ca1b9a5107a132b1682bd3d70c5080b72f45fb5
Charles Holbrow <CharlesHolbrow@gmail.com>
2019-04-22 19:55:22 -0400

    mw send osc for master track meter and position

--- mw-spring-2019/mw-spring-2019.control.js
+++ mw-spring-2019/mw-spring-2019.control.js
@@ -4,21 +4,36 @@ loadAPI(8);
 // This is useful during development.
 host.setShouldFailOnDeprecatedUse(true);
 
-host.defineController("charles", "mw-spring-2019", "0.1", "d17003f5-e8d4-4c51-91b7-3b95ff52d6fb", "audioishi");
+host.defineController("Charles", "mw-spring-2019", "0.1", "d17003f5-e8d4-4c51-91b7-3b95ff52d6fb", "audioishi");
 
 function init() {
-   // TODO: Perform further initialization here.
+   var osc = host.getOscModule();
+   var sender = osc.connectToUdpServer('127.0.0.1', 6969, null);
+   
+
    println("mw-spring-2019 initialized!");
    var transport = host.createTransport();
    var position = transport.getPosition();
    // position is a SettableBeatTimeValue
    // file:///C:/Program%20Files/Bitwig%20Studio/resources/doc/control-surface/api/a00176.html
+
+   // send osc for transport
    position.addValueObserver(function(v){
-      println(v);
+      try {
+         sender.sendMessage('/transport/position', v);
+      } catch (err) {
+         println('error sending transport position: ' + err);
+      }
    });
+
+   // send osc for track
    var masterTrack = host.createMasterTrack(1);
    masterTrack.addVuMeterObserver(127, -1, false, function(v){
-      println('level: ' + v);
+      try {
+         sender.sendMessage('/track/master/meter', v);
+      } catch (err) {
+         println("error sending level: " + err);
+      }
    });
 }
 
